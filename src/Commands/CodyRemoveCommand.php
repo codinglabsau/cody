542aba55b416b25e2b6e2bddc4ab70945e777d66
stevethomas <steve@codinglabs.io>
2025-12-05 17:20:40 +1000

    wip

--- src/Commands/CodyRemoveCommand.php
+++ src/Commands/CodyRemoveCommand.php
@@ -4,7 +4,6 @@ namespace Codinglabs\Cody\Commands;
 
 use Illuminate\Console\Command;
 use Codinglabs\Cody\Concerns\RunsCodyCommands;
-
 use function Laravel\Prompts\select;
 
 class CodyRemoveCommand extends Command
@@ -15,23 +14,29 @@ class CodyRemoveCommand extends Command
 
     protected $description = 'Remove an AI workflow';
 
-    public function handle(): void
+    public function handle(): int
     {
         $branchName = $this->branchName() ?? select(
             label: 'Select the branch to remove',
             options: $this->worktrees()->pluck('branch')->toArray(),
         );
 
+        ray($branchName);
+
         $worktree = $this->worktrees()->firstWhere('branch', $branchName);
+
+        ray($worktree);
 
         if (! $worktree) {
             $this->error("No worktree found for branch '$branchName'.");
-            return;
+            return 1;
         }
 
         $this->executeCommands([
             "git worktree remove {$worktree['path']} --force",
             "git branch -d {$worktree['branch']}",
         ]);
+
+        return 0;
     }
 }
--- tests/CodyRemoveCommandTest.php
+++ tests/CodyRemoveCommandTest.php
@@ -7,22 +7,40 @@ use Codinglabs\Cody\Concerns\RunsCodyCommands;
 
 uses(RunsCodyCommands::class);
 
-test('process is invoked', function () {
+test('error if branch cannot be found', function () {
     Process::fake();
+
+    $this->artisan('cody:remove', [
+        'branch' => 'test',
+    ])->assertFailed();
+});
+
+test('process is invoked', function () {
+    Process::fake([
+        'git worktree list' => '/Users/cody/code/app-foo-bar  900ef4420 [cody/foo-bar]',
+        'git worktree remove /Users/cody/code/app-foo-bar --force',
+//        'ls *' => 'Test "ls" output',
+    ]);
 
     Artisan::call('cody:remove', [
         'branch' => 'test',
     ]);
 
     Process::assertRan(function (PendingProcess $process) {
+        return $process->command === 'git worktree list';
+    });
+
+    Process::assertRan(function (PendingProcess $process) {
+        ray($process->command);
+
         return $process->command === sprintf('git worktree remove %s --force', $this->worktreeDirectory('test')) &&
             $process->timeout === 300 &&
             $process->environment === [];
     });
-
-    Process::assertRan(function (PendingProcess $process) {
-        return $process->command === sprintf('git branch -d %s', $this->branchName('test')) &&
-            $process->timeout === 300 &&
-            $process->environment === [];
-    });
+//
+//    Process::assertRan(function (PendingProcess $process) {
+//        return $process->command === sprintf('git branch -d %s', $this->branchName('test')) &&
+//            $process->timeout === 300 &&
+//            $process->environment === [];
+//    });
 });
